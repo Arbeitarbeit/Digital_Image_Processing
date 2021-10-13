7f1898ac031e6402f6d104602688a3fb8ac6946c
tuohzheng <tuohzheng@uiowa.edu>
2021-10-12 20:38:16 -0500

    got transformed positions
    used impixelinfo and found the points, put them in the comments in line 8~10

--- transform_script.m
+++ transform_script.m
@@ -4,3 +4,8 @@ close all;
 I = imread('tu.png');
 I_2 = imread('tub-1.png');
 I_3 = imread('tub-2.png');
+
+% pixel transformed to: (upper-left,lower-left,lower-right,upper-right)
+% tub-1: (421,1);(1,1048);(929,2608);(1346,565)
+% tub-2: (123,24);(1,929);(1280,951);(1240,1)
+
