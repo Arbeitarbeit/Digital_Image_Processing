1c00471bae4c0ff10a3721c5f598a99d89f67481
Arbeitarbeit <tuoheng.zheng@outlook.com>
2021-10-13 20:38:27 -0500

    perfectionized the image reconstructio for tub-1
    cropped the image and fixed the grey value

--- transform_script.m
+++ transform_script.m
@@ -15,9 +15,12 @@ tform = maketform('affine',T);
 trans_1 = imtransform(I_2,tform,'bilinear');
 
 % tran_1: (521,385);(347,1459);(1471,1459);(1645,385)
-T_2 = [1 0 0; 0.162 1 0; 0 0 1]
+T_2 = [1 0 0; 0.162 1 0; 0 0 1];
 tform = maketform('affine',T_2);
 trans_2 = imtransform(trans_1,tform,'bilinear');
+cropped_image_1 = imcrop(trans_2, [587,388,1708-587,1456-388]); % crop image
+
+reconstucted_1 = uint8(abs(double(255 - cropped_image_1))); % reverse grey value
 
 figure(1)
 imshow(I,[])
@@ -33,4 +36,8 @@ impixelinfo
 
 figure(4)
 imshow(trans_2,[])
+impixelinfo
+
+figure(5)
+imshow(reconstucted_1,[])
 impixelinfo
