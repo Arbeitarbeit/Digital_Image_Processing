932d75229c996b58b9ea7244b5d57bdb6fb6fa16
tuohzheng <tuohzheng@uiowa.edu>
2021-11-04 22:53:33 -0500

    final commit

--- hw7/testscript2.m
+++ hw7/testscript2.m
@@ -5,16 +5,16 @@ subplot(2,2,1); imshow(i,[])
 I = rgb2gray(i);
 I = double(I);    
 
-n = 3;
+n = 5;
 s = 0.5; %sigma
-h1 = fspecial('gaussian',[n n],s)
-A = imfilter(NI,h1);
+h1 = fspecial('gaussian',[n n],s);
+A = imfilter(I,h1);
 
 hx = [1 2 1;0 0 0;-1 -2 -1];
-Sx = imfilter(I,hx,'replicate');
+Sx = imfilter(A,hx,'replicate');
 
 hy = flipdim(hx',2);
-Sy = imfilter(I,hy,'replicate');
+Sy = imfilter(A,hy,'replicate');
 
 Emag = sqrt(Sx.^2 + Sy.^2);
 Result = zeros(size(Emag));
@@ -23,7 +23,7 @@ threshold = 50;
 Result(Emag > threshold) = 255;
 Result(Emag <= threshold) = 0;
 
-subplot(2,2,2); imshow(a,[])
+subplot(2,2,2); imshow(A,[])
 subplot(2,2,3); imshow(Emag,[])
 subplot(2,2,4); imshow(Result,[])
 
