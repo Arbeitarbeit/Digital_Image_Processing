7e2bfaf1563296f94ff5dbd55aa7505a6201b253
Arbeitarbeit <tuoheng.zheng@outlook.com>
2021-11-04 21:30:51 -0500

    Tried one
    tried one algorithm with sobel operator, failed badly

--- hw7/edge_detect_algorithm.m
+++ hw7/edge_detect_algorithm.m
@@ -1,3 +1,16 @@
 function Result = edge_detect_algorithm(I)
+    I = double(I);
+    
+%     hx = [1 2 1;0 0 0;-1 -2 -1];
+%     Sx = imfilter(I,hx,'replicate');
+%     
+%     hy = flipdim(hx',2);
+%     Sy = imfilter(I,hy,'replicate');
+% 
+%     Emag = sqrt(Sx.^2 + Sy.^2);
+%     Result = zeros(size(Emag));
+%     Result(Emag > 100) = 255;
+%     Result(Emag <= 100) = 0;
+    
 
 end
--- hw7/edge_detect_script.m
+++ hw7/edge_detect_script.m
@@ -1,18 +1,18 @@
 close all;
 clear all;
 
-I = imread('resources\A.png');
+I1 = imread('resources\A.png');
 I2 = imread('resources\B.png');
 I3 = imread('resources\C.png');
 
-subplot(2,3,1);imshow(I,[]);xlabel('A')
-subplot(2,3,2);imshow(I2,[]);xlabel('B')
-subplot(2,3,3);imshow(I3,[]);xlabel('C')
+subplot(2,3,1); imshow(I1,[]); xlabel('A')
+subplot(2,3,2); imshow(I2,[]); xlabel('B')
+subplot(2,3,3); imshow(I3,[]); xlabel('C')
 
-Result1 = edge_detect_algorithm(I);
+Result1 = edge_detect_algorithm(I1);
 Result2 = edge_detect_algorithm(I2);
 Result3 = edge_detect_algorithm(I3);
 
-subplot(2,3,4);imshow(Result1,[]);xlabel('Edge Detect A')
-subplot(2,3,5);imshow(Result2,[]);xlabel('Edge Detect B')
-subplot(2,3,6);imshow(Result3,[]);xlabel('Edge Detect C')
+subplot(2,3,4); imshow(Result1,[]); xlabel('Edge Detect A')
+subplot(2,3,5); imshow(Result2,[]); xlabel('Edge Detect B')
+subplot(2,3,6); imshow(Result3,[]); xlabel('Edge Detect C')
