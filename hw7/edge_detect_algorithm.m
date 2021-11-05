e6e1674513a8d152fd19203cc0be0f60e874fb94
tuohzheng <tuohzheng@uiowa.edu>
2021-11-04 22:04:27 -0500

    tested script

--- hw7/edge_detect_algorithm.m
+++ hw7/edge_detect_algorithm.m
@@ -1,5 +1,11 @@
-function Result = edge_detect_algorithm(I)
-    I = double(I);
+function Result = edge_detect_algorithm(i)
+    I = rgb2gray(i);
+    I = double(I);    
+    
+    I_ft = fft2(I);
+    I_ft2 = fftshift(I_ft);
+    
+    result = real(ifft2(ifftshift(I_ft2)));
     
 %     hx = [1 2 1;0 0 0;-1 -2 -1];
 %     Sx = imfilter(I,hx,'replicate');
@@ -11,6 +17,4 @@ function Result = edge_detect_algorithm(I)
 %     Result = zeros(size(Emag));
 %     Result(Emag > 100) = 255;
 %     Result(Emag <= 100) = 0;
-    
-
 end
